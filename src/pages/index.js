f8f5c61de9cdbaabcf8d03d087106f1e9c758b59
codytaft <cody.taft@gmail.com>
2020-06-05 10:10:35 -0600

    Add week 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,18 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from './styles/home.module.scss'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi Fam!</h1>
     <p>Welcome to the Song Club.</p>
-    <Link to="/week-1/">Week 1</Link>
+    <div className={styles.links}>
+      <Link to="/week-1/">Week 1</Link>
+      <Link to="/week-3/">Week 3</Link>
+    </div>
   </Layout>
 )
 
---
+++ src/pages/week-3.js
@@ -0,0 +1,65 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import SongList from '../components/SongList/index'
+import styles from './styles/week.module.scss'
+
+const SecondPage = () => {
+  const songs = [
+    {
+      submitter: 'Diana',
+      artist: 'Sixto Rodriguez,',
+      song: 'Cause',
+      link: 'https://m.youtube.com/watch?v=oKFkc19T3Dk&list=RDoKFkc19T3Dk&start_radio=1',
+    },
+    {
+      submitter: 'Yvonne',
+      artist: 'Brian Adams',
+      song: 'Have You Ever Really',
+      link: 'https://youtu.be/hq2KgzKETBw',
+    },
+    {
+      submitter: 'Angelo',
+      artist: 'John Mayer',
+      song: 'Stop This Train',
+      link: 'https://youtu.be/mS2o4q7vRFM',
+    },
+    {
+      submitter: 'Ambree',
+      artist: 'Jon Bellion',
+      song: 'Stupid Deep',
+      link: 'https://www.youtube.com/watch?v=EtlZV2EaZBQ&feature=youtu.be',
+    },
+    {
+      submitter: 'Cody',
+      artist: 'Bob Dylan',
+      song: 'Black Diamond Bay',
+      link: 'https://www.youtube.com/watch?v=WhENzrnToiU',
+    },
+    {
+      submitter: 'Pedro',
+      artist: '',
+      song: '',
+      link: '',
+    },
+    {
+      submitter: 'Ofelia',
+      artist: '',
+      song: '',
+      link: '',
+    },
+  ];
+  return (
+    <Layout>
+      <SEO title="Week One" />
+      <h1>Week 1</h1>
+      <SongList songs={songs}/>
+      <Link to="/" className={styles.link}>Go Home</Link>
+    </Layout>
+  )
+}
+
+export default SecondPage
